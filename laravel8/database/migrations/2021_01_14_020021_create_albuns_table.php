88cf03f12755abb33c910a283b2d8f1403c537c4
Fernando Viana <dev@kasterweb.com.br>
2021-01-16 01:08:49 -0400

    Criando as tabelas do banco de dados

---
+++ laravel8/database/migrations/2021_01_14_020021_create_albuns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbunsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albuns', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cantores_id')->unsigned();
+            $table->string('name');
+            $table->timestamps();
+            
+            $table->foreign('cantores_id')->references('id')->on('cantores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albuns');
+    }
+}
---
+++ laravel8/database/migrations/2021_01_16_020336_create_imagens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('cantores_id')->unsigned();
+            $table->string('imagem');
+            $table->timestamps();
+            
+            $table->foreign('cantores_id')->references('id')->on('cantores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imagens');
+    }
+}
