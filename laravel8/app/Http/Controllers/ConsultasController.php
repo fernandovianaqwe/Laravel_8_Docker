34b96ee035524c2011c6acf57508471a6fe9429b
Fernando Viana <dev@kasterweb.com.br>
2021-01-16 01:25:14 -0400

    Criando os model e o controler de consultas

---
+++ laravel8/app/Http/Controllers/ConsultasController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Http\Middleware\Jwt;
+use Illuminate\Support\Facades\Validator;
+
+class ConsultasController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('jwt', ['except' => ['login','register']]);
+    }
+
+}
