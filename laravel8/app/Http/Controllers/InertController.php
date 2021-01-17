08f32c20d09b675e853e01c9b2c5313f2e0f1283
Fernando Viana <dev@kasterweb.com.br>
2021-01-17 00:13:50 -0400

    Criando  endpoint de cadastro de cantores e refresh do token

--- laravel8/app/Http/Controllers/InertController.php
+++ laravel8/app/Http/Controllers/InertController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Cantores;
 use App\Http\Middleware\Jwt;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class InertController extends Controller
 {
@@ -13,4 +15,42 @@ class InertController extends Controller
     {
         $this->middleware('jwt', ['except' => ['login','register']]);
     }
+
+    //endpoint para cadastar cantores
+    public function cadastrarcantores(Request $request)
+    {
+        //definindo as mensagem de erros
+        $messages = [
+            'name' => 'Campo inválido.',
+            'name.required' => 'O Campo NAME é obrigatório.',
+            'estilo' => 'Campo inválido.',
+            'estilo.required' => 'O Campo PASSWORD é obrigatório.',
+        ];
+        //verificando os parametros enviados
+         $validator =  Validator::make($request->all(), [
+              'name' => ['required', 'string', 'max:255'],
+              'estilo' => ['required', 'string'],
+         ],$messages);
+
+         //verificando se encontra erros nos paramtros enviados
+         if ($validator->fails()) {
+            return response()->json($validator->messages(), 200);
+         }
+         //verificar se o cantor existe com o estilo
+         $existe = DB::table('Cantores')
+                    ->where('name', $request['name'])
+                    ->where('estilos', $request['estilo'])
+                    ->get();           
+        if(json_decode($existe)){
+            return response()->json(['error' => 'Cantor já cadastrado!'], 200);
+        }
+        //insert no banco
+         Cantores::create([
+             'name' => $request['name'],
+             'estilos' => $request['estilo'],
+         ]);
+
+        return response()->json(['message' => 'Cadastro do Cantor realizado com sucesso.']);
+    }
+
 }
--- laravel8/routes/api.php
+++ laravel8/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ConsultasController;
+use App\Http\Controllers\InertController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,9 +18,20 @@ use App\Http\Controllers\AuthController;
 */
 
 //endpoints
+//endpint para login
 Route::post('login', [AuthController::class , 'login']);
+//endpint para login
 Route::post('register', [AuthController::class , 'register']);
 
 Route::group(['middleware' => 'jwt', 'prefix' => 'auth'], function ($router) {
+    //endpint para logout
     Route::post('logout', [AuthController::class , 'logout']);
+    //endpint para refresh do token
+    Route::post('refresh', [AuthController::class , 'refresh']);
+    //endpint para cadastrar cantores
+    Route::post('cadastrarcantores', [InertController::class , 'cadastrarcantores']);
+    //endpint para cadastrar cantores
+    Route::post('cadastraalbuns', [InertController::class , 'cadastraalbuns']);
 });
+
+
